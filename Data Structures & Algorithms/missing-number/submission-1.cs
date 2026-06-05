1e395639a273efa8e5d06d55510f5d241d3c0c71
akj627 <achintya.jha@gmail.com>
2026-06-05 15:02:53 -0400

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        var sum1 = 0;
+        var sum2 = nums.Length;
+        for (var i=0;i<nums.Length;i++)
+        {
+            sum1 += nums[i];
+            sum2 += i;
+        }
+
+        return sum2 - sum1;
+    }
+}
