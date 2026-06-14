4c13e2f9743c499abadcd53f7516298e21e3f18a
akj627 <achintya.jha@gmail.com>
2026-06-14 02:04:00 -0400

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        var left = 0;
+        var right = 0;
+        var hashLen = new Dictionary<int,int>();
+        var maxLen = 0;
+        var maxOccur = 0;
+
+        while (right < s.Length)
+        {
+            var charType = s[right] - 'A';
+
+            if (hashLen.ContainsKey(charType))
+                hashLen[charType]++;
+            else
+                hashLen.Add(charType, 1);
+
+            maxOccur = Math.Max(maxOccur, hashLen[charType]);
+            
+            if (right - left + 1 - maxOccur > k)
+            {
+                hashLen[s[left] - 'A']--;
+                hashLen[s[right] - 'A']--;
+                left++;
+            }
+            else
+            {
+                maxLen = Math.Max(maxLen, right - left + 1);
+                right++;
+            }
+        }
+
+        return maxLen;
+    }
+}
